c3dc340fd85b870ed54cc3ec9824f83b992e9411
Muhammed Emin BARKOÇ <eminbarkoc@gmail.com>
2023-03-19 22:30:06 +0300

    Boss Ayarlamaları
    Boss animasyonları yapıldı.
    Boss spawn mekaniği yapıldı.
    Boss sağlık mekaniğinde hata tespit edildi.

--- 2D Metroidvania Game/Assets/Scripts/BossBattle.cs
+++ 2D Metroidvania Game/Assets/Scripts/BossBattle.cs
@@ -8,17 +8,65 @@ public class BossBattle : MonoBehaviour
     public Transform camPosition;
     public float camSpeed;
 
+    public int treshold1, treshold2;
+
+    public float activeTime, fadeoutTime, inactiveTime;
+    private float activeCounter, fadeCounter, inactiveCounter;
+
+    public Transform[] spawnPoints;
+    private Transform targetPoint;
+    public float moveSpeed;
+
+    public Animator anim;
+
+    public Transform theBoss;
 
     private void Start()
     {
         theCam = FindObjectOfType<CameraController>();
         theCam.enabled = false;
+
+        activeCounter = activeTime;
     }
 
 
     private void Update()
     {
         theCam.transform.position = Vector3.MoveTowards(theCam.transform.position, camPosition.position, camSpeed * Time.deltaTime);
+   
+        if (BossHealthController.instance.currentHealth > treshold1)
+        {
+            if (activeCounter > 0)
+            {
+                activeCounter -= Time.deltaTime;
+                if (activeCounter <= 0)
+                {
+                    fadeCounter = fadeoutTime;
+                    anim.SetTrigger("vanish");
+                }
+            }
+            else if (fadeCounter > 0)
+            {
+                fadeCounter -= Time.deltaTime;
+                if (fadeCounter <= 0)
+                {
+                    theBoss.gameObject.SetActive(false);
+                    inactiveCounter = inactiveTime;
+                }
+            }
+            else if (inactiveCounter > 0)
+            {
+                inactiveCounter -= Time.deltaTime;
+                if (inactiveCounter <= 0)
+                {
+                    theBoss.position = spawnPoints[Random.Range(0, spawnPoints.Length)].position;
+                    theBoss.gameObject.SetActive(true);
+
+                    activeCounter = activeTime;
+                }
+
+            }
+        }
     }
 
     public void BattleEnd()
